2091a1912db37c89f8fffe53300088cde5e0ac03
Julie Hervias Mendes Leal <juliehmleal@gmail.com>
2025-04-18 13:15:39 -0300

    Mudança no visual e nas pastas

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcTeste.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
